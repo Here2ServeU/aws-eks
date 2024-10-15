3ef7a40cda53c5b0c1e0897417255970d4c46164
LearnDevOps&Cloud <revnaweji@gmail.com>
2024-10-15 11:04:33 -0500

    Create steps.sh

---
+++ terraform/steps.sh
@@ -0,0 +1,16 @@
+### Move to the folder, terraform, after cloning this repository
+* cd /terraform/ 
+
+### Modify the variables.tf file as desired
+
+### Initialize
+* terraform init
+
+### Plan 
+* terraform plan
+
+### Apply
+* terraform apply 
+
+### Clean Up
+* terraform destroy
