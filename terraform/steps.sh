a8308b8d10f319a061b3408cf66f4216e66f0d3c
LearnDevOps&Cloud <revnaweji@gmail.com>
2024-10-15 11:07:48 -0500

    Update steps.sh

--- terraform/steps.sh
+++ terraform/steps.sh
@@ -1,16 +1,43 @@
-### Move to the folder, terraform, after cloning this repository
+# Move to the folder, terraform, after cloning this repository
 cd /terraform/ 
 
-### Modify the variables.tf file as desired
+# Modify the variables.tf file as desired
 
-### Initialize
+# Initialize
 terraform init
 
-### Plan 
+# Plan 
 terraform plan
 
-### Apply
+# Apply
 terraform apply 
+
+# Update kubeconfig
+aws eks update-kubeconfig --region <region> --name <cluster-name>
+
+# Verify that kubectl can access the cluster by running
+kubectl get nodes
+
+# Apply the deployment to your cluster
+kubectl apply -f nginx-deployment.yaml
+
+
+# kubectl apply -f nginx-configmap.yaml
+kubectl apply -f nginx-configmap.yaml
+
+# kubectl apply -f nginx-service.yaml
+kubectl apply -f nginx-service.yaml
+
+# After a few minutes, obtain the external IP of the Nginx service. 
+# Note the EXTERNAL-IP in the output.
+kubectl get svc nginx-service
+
+# Verify the Deployment
+# Open a web browser and navigate to the external IP obtained in the previous step.
+# You should see the message:
+# "Hello from T2S. If you see this message, you have successfully set up a Kubernetes cluster on AWS using EKS. 
+# You are a genius, and God bless you!"
+
 
 ### Clean Up
 terraform destroy
